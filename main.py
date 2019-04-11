e16cfafbff4321c49366a040fc8dc21ac5975b22
Alex Yovev <ayovev511@gmail.com>
2019-04-10 21:43:17 -0700

    got client and server classes setup and connected to socket

---
+++ main.py
@@ -0,0 +1,19 @@
+import socket, threading
+
+class Client(threading.Thread):
+    def run(self):
+        clientHost = "127.0.0.1"
+        clientPort = 12345
+        clientAddress = (clientHost, clientPort)
+
+        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class Server(threading.Thread):
+    def run(self):
+        count = 0
+        serverHost = "127.0.0.1"
+        serverPort = 12346
+        serverAddress = (serverHost, serverPort)
+
+        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        connection.bind(serverAddress)
