39a9e68b2b6cc8ecfb75b743e3ca9061f8341f8d
Alex Yovev <ayovev511@gmail.com>
2019-04-14 19:13:17 -0700

    created attacker class

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ class Server(threading.Thread):
 
         connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         connection.bind(serverAddress)
+
+class Attacker(threading.Thread):
+    def run(self):
+        clientHost = "127.0.0.1"
+        clientPort = 12345
+        clientAddress = (clientHost, clientPort)
+
+        clientConnection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientConnection.bind(clientAddress)
+
+        serverHost = "127.0.0.1"
+        serverPort = 12346
+        serverAddress = (serverHost, serverPort)
+        
+        serverConnection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
